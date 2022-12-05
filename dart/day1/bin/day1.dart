3e95529c915a398b48002665476a68d978e85b0e
bernsky <bernhardrobl@gmx.de>
2022-12-05 21:06:52 +0100

    added day1 dart project

---
+++ dart/day1/bin/day1.dart
@@ -0,0 +1,5 @@
+import 'package:day1/day1.dart' as day1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${day1.calculate()}!');
+}
---
+++ dart/day1/test/day1_test.dart
@@ -0,0 +1,8 @@
+import 'package:day1/day1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
